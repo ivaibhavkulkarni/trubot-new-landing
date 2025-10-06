0a3d51218a8db80be9088cc98fa50fd3d4802cc7
ivaibhavkulkarni <vaibhav.kulkarni0359@gmail.com>
2025-10-06 09:51:32 +0530

    initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./pages/Header/Header"
+import HeroSection from "./pages/Hero/HeroSection"
+import OurServices from "./pages/OurServices/Ourservices"
+import AboutUs from "./pages/AboutUs/AboutUs"
+import Footer from "./pages/Footer/Footer"
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+      <HeroSection/>
+      <OurServices/>
+      <AboutUs/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/OurServices/Ourservices.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const OurServices = () => {
+  return (
+    <div>services</div>
+  )
+}
+
+export default OurServices
